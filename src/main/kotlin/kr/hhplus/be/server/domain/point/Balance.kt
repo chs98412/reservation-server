dd406b86b5adb9aa0c30979ee860f38c56523e10
최혁순 <choehyeogsun@choehyeogsun-ui-MacBookPro.local>
2025-07-27 23:17:56 +0900

    [#27] feat: 잔액 조회

--- src/main/kotlin/kr/hhplus/be/server/domain/point/Balance.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/point/Balance.kt
@@ -1,11 +1,29 @@
 package kr.hhplus.be.server.domain.point
 
+import jakarta.persistence.*
 import kr.hhplus.be.server.common.exception.InsufficientBalanceException
+import java.time.LocalDateTime
 
+@Entity
+@Table(name = "balance")
 class Balance(
-    val accountId: Long,
+
+    @Id
+    @Column(name = "balance_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val balanceId: Long,
+
+    @Column(name = "account_id")
+    val accountId: String,
+
+    amount: Long,
 ) {
-    private var amount: Long = 0
+    @Column(name = "amount")
+    var amount = amount
+        private set
+
+    @Column(name = "updated_at")
+    val updatedAt: LocalDateTime = LocalDateTime.now()
 
     fun charge(amount: Long) {
         this.amount += amount
@@ -15,6 +33,4 @@ class Balance(
         if (amount < price) throw InsufficientBalanceException()
         amount -= price
     }
-
-    fun getAmount(): Long = amount
 }
--- src/test/java/kr/hhplus/be/server/domain/point/BalanceTest.kt
+++ src/test/java/kr/hhplus/be/server/domain/point/BalanceTest.kt
@@ -8,13 +8,13 @@ import kr.hhplus.be.server.common.exception.InsufficientBalanceException
 class BalanceTest : BehaviorSpec({
 
     Given("초기 금액이 0인 Balance가 있을 때") {
-        val balance = Balance(accountId = 1L)
+        val balance = Balance(balanceId = 1L, accountId = "accountId", amount = 0L)
 
         When("100원을 충전하면") {
             balance.charge(100)
 
             Then("잔액은 100원이 된다") {
-                balance.getAmount() shouldBeExactly 100L
+                balance.amount shouldBeExactly 100L
             }
         }
 
@@ -22,12 +22,12 @@ class BalanceTest : BehaviorSpec({
             balance.charge(50)
 
             Then("잔액은 150원이 된다") {
-                balance.getAmount() shouldBeExactly 150L
+                balance.amount shouldBeExactly 150L
             }
         }
     }
     Given("잔액이 부족한 Balance가 주어졌을 때") {
-        val balance = Balance(accountId = 2L)
+        val balance = Balance(balanceId = 1L, accountId = "accountId", amount = 1L)
 
         When("100원을 차감하면") {
             Then("InsufficientBalanceException이 발생해야 한다") {
