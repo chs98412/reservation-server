d26e78b01062ec77dcf3fd2938752109ea02c933
최혁순 <choehyeogsun@choehyeogsun-ui-MacBookPro.local>
2025-07-31 21:38:32 +0900

    [#27] test: pay 테스트 코드

--- src/main/kotlin/kr/hhplus/be/server/application/point/ChargePointService.kt
+++ src/main/kotlin/kr/hhplus/be/server/application/point/ChargePointService.kt
@@ -3,11 +3,13 @@ package kr.hhplus.be.server.application.point
 import kr.hhplus.be.server.common.exception.NotFoundBalanceException
 import kr.hhplus.be.server.domain.point.BalanceRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ChargePointService(
     private val balanceRepository: BalanceRepository,
 ) : ChargePointUseCase {
+    @Transactional
     override fun execute(accountId: String, amount: Long) {
         balanceRepository.findByAccountId(accountId)?.also {
             it.charge(amount)
